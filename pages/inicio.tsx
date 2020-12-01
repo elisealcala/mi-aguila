3fcd3cb4120862dd3222d8002c2263f81a250be6
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2020-12-01 17:59:13 -0500

    feat: create inicio page and screen

---
+++ pages/inicio.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HomeScreen from '../src/screens/home-screen'
+
+const Home = () => {
+  return (
+    <HomeScreen />
+  )
+}
+
+export default Home
---
+++ src/screens/home-screen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../layout/main'
+
+const HomeScreen = () => {
+
+  return (
+    <MainLayout>
+      Inicio
+    </MainLayout>
+  )
+}
+
+export default HomeScreen
